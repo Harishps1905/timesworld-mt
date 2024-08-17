b91aa086db84abeab85dbfbf0fe4c6a723b6ccaf
Harish Swaminathan <harishswami62@gmail.com>
2024-08-17 05:33:58 +0530

    firebase updated

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD6ReFJ8_C0WORQBkpgw7Zs9ozH_AjLeLs",
+  authDomain: "timesworld-mt.firebaseapp.com",
+  projectId: "timesworld-mt",
+  storageBucket: "timesworld-mt.appspot.com",
+  messagingSenderId: "482665756481",
+  appId: "1:482665756481:web:87d04b1b7eb1d6d38ed3c0",
+  measurementId: "G-S07ZG6SJ2R"
+};
+
+const analytics = getAnalytics(app);
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
