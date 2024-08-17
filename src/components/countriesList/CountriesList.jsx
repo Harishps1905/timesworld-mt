820ffa44ae5a00cca7cf1080d63f425b2fb5ed01
Harish Swaminathan <harishswami62@gmail.com>
2024-08-17 05:48:13 +0530

    fixed countrieslist

--- src/components/countriesList/CountriesList.jsx
+++ src/components/countriesList/CountriesList.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { countriesState, fetchCountries } from "../../store/countriesSlice";
 import { Col, Container, Row } from "react-bootstrap";
-import './Countrieslist.css';
+import './countrieslist.css';
 import Country from "../country/Country";
 
 
