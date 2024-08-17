ea6d4f8987fea5dbe02d118c8b6c634b1f0c8b9b
Harish Swaminathan <harishswami62@gmail.com>
2024-08-17 05:36:01 +0530

    pr test

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './styles/index.css'
 import { Provider } from 'react-redux';
 import {store} from "./store/store";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
