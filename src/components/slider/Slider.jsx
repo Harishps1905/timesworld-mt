3ed52639615d586c59b4e864c9dbd3f196a9ed85
Harish Swaminathan <harishswami62@gmail.com>
2024-08-17 16:01:45 +0530

    updated auth login

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -3,30 +3,26 @@ import { Col, Container, Row } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 import './Slider.css';
 import countriesflag from './assets/countries-flag.jpg';
-import garlandbanners from './assets/garland-banners.webp';
-import hokkaidocountry from './assets/pngtree-winter-scenery-high-place-hokkaido-country-background-banner-hd-image_16003975.jpg';
+import countriesflag2 from './assets/countries-flag2.jpg';
+import countriesflag3 from './assets/countries-flag3.jpg';
 const Slider = () => {
   return (
-    <div>
-    <Container>
-        <Row>
-            <Col>
-                <Carousel>
-                    <Carousel.Item>
-                        <img class="d-block w-100"  src={countriesflag} alt="First slide" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img class="d-block w-100"  src={garlandbanners} alt="Second slide" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img class="d-block w-100"  src={hokkaidocountry} alt="Third slide" />
-                    </Carousel.Item>
-                </Carousel>
+    <>
+        <Col xs={12} md={{ order: 'first', span: 9  }}>
+            <Carousel className='h-100'>
+                <Carousel.Item className='h-100'>
+                    <img className="d-block w-100 h-100"  src={countriesflag} alt="First slide" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className="d-block w-100 h-100"  src={countriesflag2} alt="Second slide" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className="d-block w-100 h-100"  src={countriesflag3} alt="Third slide" />
+                </Carousel.Item>
+            </Carousel>
 
-            </Col>
-        </Row>
-    </Container>
-    </div>
+        </Col>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,24 +4,32 @@ import Welcome from "../components/welcome/Welcome";
 import Banner from "../components/banner/Banner";
 import Slider from "../components/slider/Slider";
 import CountriesList from "../components/countriesList/CountriesList";
-import flagsImage from'../components/banner/assets/flags.jpg';
+import flagsImage from "../components/banner/assets/flags.jpg";
 import useAuth from "../hooks/useAuth";
 import { userSignOut } from "../firebase/auth";
 import Footer from "../components/footer/Footer";
+import withAuth from "../hoc/withAuth";
+import { Container, Row } from "react-bootstrap";
+
 function Home() {
   // userSignOut();
   const { user, loading, error } = useAuth();
   console.log(user, "Welcome");
-  
-  
+  const AuthCountriesList = withAuth(CountriesList);
+
   return (
     <>
       <Navigationbar />
       <Welcome />
-      <Banner bimage={flagsImage}/>
-      <Slider />
-      {user && <CountriesList/>}
-      <Footer/>
+
+      <Container>
+        <Row>
+          <Banner bimage={flagsImage} />
+          <Slider />
+        </Row>
+      </Container>
+      <AuthCountriesList />
+      <Footer />
     </>
   );
 }
